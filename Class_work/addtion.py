2e7834099268b364bb779610a6c75fe6980c3c15
Silki <silkichauhan057@gmail.com>
2026-03-17 10:23:04 +0530

    Add files via upload

---
+++ Class_work/addtion.py
@@ -0,0 +1,10 @@
+# def addnumber(x,y):
+#     print("sum of ",x,"and",y,"is",(x,y))
+
+
+def add(x,y):
+    return(x+y)
+x=int(input("enter the value of  x="))
+y=int(input("enter the value of  y=4" \
+""))
+print("sum :",add(x,y)) 
