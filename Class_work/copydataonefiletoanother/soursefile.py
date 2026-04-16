4d43c7a8d6f015f99c9d2881dbb1ea181f1ed71d
Silki <silkichauhan057@gmail.com>
2026-04-16 10:22:31 +0530

    Delete Class_work/copydataonefiletoanother directory

--- Class_work/copydataonefiletoanother/soursefile.py
+++
@@ -1,5 +0,0 @@
-# Create source file and add content
-with open("source.txt", "w") as f:
-    f.write("Hello! This is the source file.\nData will be copied from here.")
-
-print("Source file ready")
